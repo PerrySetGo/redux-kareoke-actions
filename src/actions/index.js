e5615f4120313b615056b4a92cf2533a8b8e27b3
perry eising <perry.r.eising@gmail.com>
2018-07-06 22:34:43 -0700

    add code to make API call - middleware still missing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,18 @@
 import * as types from "./../constants/ActionTypes";
+import v4 from 'uuid/v4';
+
+const apiKey = '35d8b1f6e859a83becf304830e7267e1';
+
+export function fetchSongId(title) {
+  return function (dispatch){
+    const localSongId = v4();
+    title = title.replace(' ', '_');
+    return fetch (`http://api.musixmatch.com/ws/1.1/track.search?&q_track=${title}&page_size=1&s_track_rating=desc&apikey=${apiKey}`)
+    .then(response => response.json(), error => console.log('An error occurred', error)).then(function(json){
+      console.log('Response yo', json)
+    });
+  };
+}
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -13,4 +27,10 @@ export const restartSong = (currentSongId) => ({
 export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
+});
+
+export const requestSong = (title, localSongId) => ({
+  type: types.REQUEST_SONG,
+  title,
+  songId:localSongId
 });
